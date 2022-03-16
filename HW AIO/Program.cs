5c4f3f7264e120c6ee24adfbfcbe4752c1d8ede2
Emil <intestinal_d@rambler.ru>
2022-03-17 02:17:05 +0300

    Added test runs for Variables, Branches and Cycles Methods

--- HW AIO/Program.cs
+++ HW AIO/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HW_AIO;
+
+//Variables.GetADividedByB(0, 2);                                                               //1.1
+
+//Console.WriteLine(Variables.GetSolutionOfQuadraticEquation(6, 3));                            //1.2
+
+//Variables.SwapAtoB("string", "32132");                                                        //1.3
+
+//Console.WriteLine(Variables.GetSolutionOfLinearEquation(3, 5, 11));                           //1.4
+
+//Variables.GetLinearEquation(1, 1, 3, 4);                                                      //1.5
+
+
+///*_____________________________________________________________________________________________*/
+
+//Console.WriteLine(Branches.GetSumOfAAndB(3, 2));                                              //2.1
+
+//Console.WriteLine(Branches.GetQuarterOfPoint(7.25, -3));                                      //2.2
+
+//Branches.GetNumbersInAscendingOrder(111, 0, -232.5);                                          //2.3
+
+//Branches.GetSolutionOfQuadraticEquation(1, 0, -49);                                           //2.4
+
+//Console.WriteLine(Branches.GetNumberInWords(-29));                                            //2.5
+
+
+///*_____________________________________________________________________________________________*/
+//Console.WriteLine(Cycles.GetAInDegreeOfB(3, 3));                                              //3.1
+
+//Cycles.GetNumbersIfDivisionByAIsZero(25);                                                     //3.2
+
+//Console.WriteLine(Cycles.GetCountOfNumbersIfTheirSquareIsLowerThanA(17));                     //3.3
+
+//Console.WriteLine(Cycles.GetMaxDividerOfA(135));                                              //3.4
+
+//Console.WriteLine(Cycles.GetSumOfNumbersFromAToBWhichDivisionBySevenIsZero(100, 4));          //3.5
+
+//Console.WriteLine(Cycles.GetPositonValueFromFibonacci(7));                                    //3.6
+
+//Console.WriteLine(Cycles.GetMaxDividerOfTwoNumbersByEuclid(48, 32));                          //3.7
+
+//Console.WriteLine(Cycles.GetThirdRootFromNumberByHalfDivision(512));                          //3.8
+
+//Console.WriteLine(Cycles.GetCountOfEvenDigitsInNumber(3236));                                 //3.9
+
+//Console.WriteLine(Cycles.GetReversedNumber(1234));                                            //3.10
+
+//Cycles.WriteNumbersIfEvenDigitsSumBiggerThanOdd(1234);                                        //3.11
+
+//Console.WriteLine(Cycles.AreNumbersHavingSameDigits(-1000, 1024));                            //3.12
