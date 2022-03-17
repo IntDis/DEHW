e446e317938bcad7a58e6dee6d12b20669b1af5c
Emil <intestinal_d@rambler.ru>
2022-03-18 01:12:24 +0300

    Added ArrayMethods class

---
+++ HW AIO/ArrayMethods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_AIO
+{
+    public class ArrayMethods
+    {
+        public static void PrintArrayElements(int[] arr)
+        {
+            foreach(int i in arr)
+            {
+                Console.Write($"{i} ");
+            }
+        }
+    }
+}
