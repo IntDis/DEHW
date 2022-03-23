3d68cce0319c324b0a92bfcdde9425a40e350ec1
Emil <intestinal_d@rambler.ru>
2022-03-24 02:25:55 +0300

    Created DoubleArray class for double array methods

---
+++ HW AIO/DoubleArray.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_AIO
+{
+    public class DoubleArray
+    {
+        ////public static int GetMaxElement(int[] array)
+        ////{
+
+        ////}
+    }
+}
